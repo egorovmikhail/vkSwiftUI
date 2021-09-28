0e6740c43a18cc396029c76a513a81ba7ac0f798
Михаил <egorovmikhail@bk.ru>
2021-09-28 19:20:36 +1100

    Создал проект

---
+++ vkSwiftUI/vkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkSwiftUIApp.swift
+//  vkSwiftUI
+//
+//  Created by Михаил Егоров on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct vkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
